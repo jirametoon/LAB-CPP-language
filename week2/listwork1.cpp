66d5fb298ab1efa1568db0c0591b081a01bb6bdb
jirametoon <sendtocartoon@gmail.com>
2023-12-08 15:57:04 +0700

    listwork

---
+++ week2/listwork1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int	main(void)
+{
+	size_t		m, num;
+	char	c;
+	cin >> m;
+
+	vector<int>	lst;
+
+	for (size_t i = 0; i < m; i++)
+	{
+		cin >> c >> num;
+		if (c == 'I')
+			lst.insert(lst.begin(), num);
+		else if (c == 'D')
+			if (num <= lst.size())
+				lst.erase(lst.begin() + num - 1);
+	}	
+
+	for (int val : lst)
+		cout << val << "\n";
+	return (0);
+}
+
